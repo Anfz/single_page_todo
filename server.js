24d6f1669994e2590b55ababe1f99248db953fc0
Christopher Beaver <chrisbvr@gmail.com>
2016-02-14 23:49:29 +0000

    Basic todo angular front end

--- server.js
+++ server.js
@@ -79,6 +79,12 @@
             });
         });
     });
+    
+    
+    // application -------------------------------------------------------------
+    app.get('*', function(req, res) {
+        res.sendfile('./client/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+    });
 
     // listen (start app with node server.js) ======================================
     app.listen(8080);
--- server/model/todo.js
+++ server/model/todo.js
@@ -4,6 +4,6 @@ var mongoose = require('mongoose');
 var Todo = mongoose.model('Todo', {
     text : String
 });
-
+ 
 
 module.exports = Todo;
